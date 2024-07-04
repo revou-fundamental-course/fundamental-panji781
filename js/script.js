0a0315c46f4fb44a969c74014793f7c8d8e16ea3
panji781 <panji_rsbi@yahoo.com>
2024-07-05 00:27:00 +0800

    selesai basic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 //  Ini Javascript
+
+function validateform() {
+    let namapertama = document.getElementById('namapertama').Value;
+    let namakedua = document.getElementById('namakedua').Value;
+
+    console.log(namapertama);
+    console.log(namakedua);
+
+
+}
